b15b6552b0cc686b02efe4e47cd6a6a4009e1ea9
archds <dsalekseev11@gmail.com>
2022-04-10 02:42:12 +0700

    initial

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+mod settings;
+
+extern crate fs_extra;
+extern crate fstrings;
+extern crate home;
+extern crate humansize;
+extern crate winrt_notification;
+
+use core::panic;
+
+use fs_extra::dir::get_size;
+use home::home_dir;
+use humansize::{file_size_opts as fs_options, FileSize};
+use settings::load_settings;
+use winrt_notification::Toast;
+
+pub struct Size {
+    pub bytes: u64,
+}
+
+impl Size {
+    fn humansize(&self) -> String {
+        return self.bytes.file_size(fs_options::DECIMAL).unwrap();
+    }
+}
+
+fn notify(size: Size, dirname: &String) {
+    let message =
+        String::from(dirname) + " directory exceeded " + size.humansize().as_str() + " limit";
+
+    Toast::new("Microsoft.WindowsTerminal_8wekyb3d8bbwe!App")
+        .title("Roast")
+        .text1(message.as_str())
+        .show()
+        .expect("unable to toast");
+}
+
+fn main() {
+    let path = home_dir()
+        .expect("Failed to locate home dir!")
+        .join("Downloads");
+
+    if !path.exists() {
+        panic!("Downloads path not found in {}!", path.to_str().unwrap())
+    }
+
+    let downloads_folder_size = get_size(home_dir().unwrap().join("repos")).unwrap();
+    let settings = load_settings();
+
+    if downloads_folder_size > settings.size_limit.bytes {
+        notify(
+            settings.size_limit,
+            &String::from(path.file_name().unwrap().to_str().unwrap()),
+        )
+    }
+}
---
+++ src/settings.rs
@@ -0,0 +1,43 @@
+extern crate ini;
+
+use ini::Ini;
+
+use crate::Size;
+
+pub struct Settings {
+    pub size_limit: Size,
+}
+
+const INI_PATH: &str = "settings.ini";
+const DEFAULT_SIZE_LIMIT: u64 = 1024 * 1024;
+
+pub fn load_settings() -> Settings {
+    let settings = match Ini::load_from_file(INI_PATH) {
+        Ok(ini) => Settings {
+            size_limit: Size {
+                bytes: ini
+                    .get_from(Some("Main"), "size_limit")
+                    .expect("Failed to load size_limit property!")
+                    .parse::<u64>()
+                    .expect("Failed to parse size_limit property as integer!"),
+            },
+        },
+        Err(_) => {
+            let mut conf = Ini::new();
+
+            conf.with_section(Some("Main"))
+                .set("size_limit", DEFAULT_SIZE_LIMIT.to_string().as_str());
+
+            conf.write_to_file(INI_PATH)
+                .expect("Failed to write config file!");
+
+            Settings {
+                size_limit: Size {
+                    bytes: DEFAULT_SIZE_LIMIT,
+                },
+            }
+        }
+    };
+
+    return settings;
+}
